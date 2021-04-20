f7ca8f7e8c67d21a83d1b43dd1ca26164eed13d1
harshitTTN <harshit.abrol@tothenew.com>
2021-04-20 10:58:07 +0530

    Unit Testing

--- core/src/test/java/com/adobe/aem/guides/bpit4622/core/models/impl/BylineImplTest.java
+++ core/src/test/java/com/adobe/aem/guides/bpit4622/core/models/impl/BylineImplTest.java
@@ -1,47 +1,134 @@
 package com.adobe.aem.guides.bpit4622.core.models.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mock;
 
 import com.adobe.aem.guides.bpit4622.core.models.Byline;
+import com.adobe.cq.wcm.core.components.models.Image;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 
-@ExtendWith(AemContextExtension.class)
-public class BylineImplTest {
+import org.apache.sling.models.factory.ModelFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import org.apache.sling.api.resource.Resource;
+import java.util.List;
+import com.google.common.collect.ImmutableList;
+
+@ExtendWith({AemContextExtension.class,  MockitoExtension.class})
+class BylineImplTest {
     private final AemContext ctx = new AemContext();
 
+    @Mock
+    private Image image;
+
+    @Mock
+    private ModelFactory modelFactory;
 
     @BeforeEach
     void setUp() throws Exception {
         ctx.addModelsForClasses(BylineImpl.class);
-    ctx.load().json("/com/adobe/aem/guides/bpit4622/core/models/impl/BylineImplTest.json", "/content");
 
+        ctx.load().json("/com/adobe/aem/guides/bpit4622/core/models/impl/BylineImplTest.json", "/content");
+
+        lenient().when(modelFactory.getModelFromWrappedRequest(eq(ctx.request()), any(Resource.class), eq(Image.class)))
+                .thenReturn(image);
+
+        ctx.registerService(ModelFactory.class, modelFactory, org.osgi.framework.Constants.SERVICE_RANKING,
+                Integer.MAX_VALUE);
     }
 
-    @Test 
-    void testGetName() { 
+    @Test
+    void testGetName() {
         final String expected = "Jane Doe";
 
         ctx.currentResource("/content/byline");
         Byline byline = ctx.request().adaptTo(Byline.class);
-    
+
         String actual = byline.getName();
-    
+
         assertEquals(expected, actual);
     }
-    
-    @Test 
-    void testGetOccupations() { 
-        fail("Not yet implemented");
+
+    @Test
+    void testGetOccupations() {
+        List<String> expected = new ImmutableList.Builder<String>()
+                .add("Blogger")
+                .add("Photographer")
+                .add("YouTuber")
+                .build();
+
+        ctx.currentResource("/content/byline");
+        Byline byline = ctx.request().adaptTo(Byline.class);
+
+        List<String> actual = byline.getOccupations();
+
+        assertEquals(expected, actual);
     }
 
-    @Test 
-    void testIsEmpty() { 
-        fail("Not yet implemented");
+    @Test
+    public void testIsEmpty() {
+        ctx.currentResource("/content/empty");
+
+        Byline byline = ctx.request().adaptTo(Byline.class);
+
+        assertTrue(byline.isEmpty());
     }
+
+    @Test
+    public void testIsEmpty_WithoutName() {
+        ctx.currentResource("/content/without-name");
+
+        Byline byline = ctx.request().adaptTo(Byline.class);
+
+        assertTrue(byline.isEmpty());
+    }
+
+    @Test
+    public void testIsEmpty_WithoutOccupations() {
+        ctx.currentResource("/content/without-occupations");
+
+        Byline byline = ctx.request().adaptTo(Byline.class);
+
+        assertTrue(byline.isEmpty());
+    }
+
+    @Test
+    public void testIsEmpty_WithoutImage() {
+        ctx.currentResource("/content/byline");
+
+        lenient().when(modelFactory.getModelFromWrappedRequest(eq(ctx.request()),
+                any(Resource.class),
+                eq(Image.class))).thenReturn(null);
+
+        Byline byline = ctx.request().adaptTo(Byline.class);
+
+        assertTrue(byline.isEmpty());
+    }
+
+    @Test
+    public void testIsEmpty_WithoutImageSrc() {
+        ctx.currentResource("/content/byline");
+
+        when(image.getSrc()).thenReturn("");
+
+        Byline byline = ctx.request().adaptTo(Byline.class);
+
+        assertTrue(byline.isEmpty());
+    }
+    @Test
+    public void testIsNotEmpty() {
+        ctx.currentResource("/content/byline");
+        when(image.getSrc()).thenReturn("/content/bio.png");
+
+        Byline byline = ctx.request().adaptTo(Byline.class);
+
+        assertFalse(byline.isEmpty());
+    }
+
 }
